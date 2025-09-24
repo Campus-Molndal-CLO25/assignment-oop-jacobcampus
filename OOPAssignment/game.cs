431450f9a1750e9585c60edb3327efbda07d4428
jacob mayner <05jacmay@gafe.molndal.se>
2025-09-24 19:09:14 +0200

    Add game loop, input handling, and room updates
    The `Game` class was enhanced with a game loop in the `Start` method, allowing continuous input processing until `gameOver` is true. A new `Input` method was added to handle commands, starting with the "hjälp" command for displaying help.
    
    The `CreateWorld` method was updated to include new rooms (`skog` and `mörkskog`) and set the `currentRoom` to `start`. Minor adjustments were made to item descriptions.
    
    Additionally, a `Description` property was added to the `Game` class, and a comment was added to the `using System.Collections.Generic;` directive for clarity.

--- OOPAssignment/game.cs
+++ OOPAssignment/game.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Generic;//avänds för listan
 
 public class Program
 {
@@ -17,13 +17,22 @@ public class Game
     public Dictionary<string, Room> rooms = new Dictionary<string, Room>();
     public List<Item> inventory = new List<Item>();
     public Room currentRoom;
+    public string Description;
     public bool gameOver = false;
     public void Start()
     {
         CreateWorld();
         Console.WriteLine(currentRoom.Name);
         Console.WriteLine(currentRoom.Description);
+        while (!gameOver)
+        {
+            Console.WriteLine(currentRoom.Description);
+            Console.Write("> ");
+            string input = Console.ReadLine().ToLower();
+            Input(input);
 
+            
+        }
 
 
     }
@@ -31,15 +40,28 @@ public class Game
     {
         
         var svärd = new Item("svärd", "ett silver svärd.");
-        var diamant = new Item("diamant", "en stort diamant – victory");
+        var diamant = new Item("diamant", "en stort diamant =victory");
 
         Room start = new Room("start", "du står vid en ruin.", new List<Item>());
         Room skogen = new Room("skog", "du står i en mörk skog.", new List<Item>());
         Room mörkskog = new Room("grotta", "du är i en mörkare skog och ser ett svärd.", new List<Item> { svärd, });
-        
+
+        rooms["start"] = start;
+        rooms["skog"] = skogen;
+        rooms["mörkskog"] = mörkskog;
+
+      
+
         currentRoom = start;
     }
-   
-    
-   
+
+
+    private void Input(String input) 
+    {
+        if (input == "hjälp")
+       {
+
+            Console.WriteLine("du kan gå i en riktning du kan skriva inventory för att visa inverntory du kan ta item");
+        }
+    }
 }
